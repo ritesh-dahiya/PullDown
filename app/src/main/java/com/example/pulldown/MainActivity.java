02ff48cf3f028ffdbc403d595213c8953f02f60d
Ritesh <ritesh.99981@gmail.com>
2019-11-13 15:18:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pulldown/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pulldown;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        //Testing the PullDown Feature of android Studio and Github
+
+
+
+
+
+
+    }
+}
